21ef8a17000b905cc0b871f51fe696afacfaa1aa
Joel Damato <jota@MacBook-Pro-de-Joel.local>
2025-10-14 20:49:08 -0300

    Configurar start.sh optimizado para Render

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# --- Variables de entorno ---
+export RENDER=true
+export PORT=10000
+export NODE_OPTIONS="--max-old-space-size=256 --expose-gc"
+
+# --- Iniciar bot ---
 node index.js
